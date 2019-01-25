39f7041c6918a07eb08f4abaa158abcf915c6a01
Issam SAID <isaid@nvidia.com>
2019-01-25 19:41:05 +0100

    add docker script to db

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p src/tmp/db
+mkdir -p ./src/tmp/db
 docker-compose -f development.yml build         \
 						  --build-arg ctr_user=$USER        \
               --build-arg ctr_group=$(id -g -n) \
