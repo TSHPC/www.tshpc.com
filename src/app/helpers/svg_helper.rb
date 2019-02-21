a6a3a56843dee3dae4609f0f654f2626d284e2d6
Issam SAID <isaid@nvidia.com>
2019-02-21 22:33:50 +0100

    add banner

---
+++ src/app/helpers/svg_helper.rb
@@ -0,0 +1,9 @@
+# app/helpers/svg_helper.rb
+
+module SvgHelper
+  def svg_tag(name)
+    file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
+    return File.read(file_path).html_safe if File.exists?(file_path)
+    '(not found)'
+  end
+end
